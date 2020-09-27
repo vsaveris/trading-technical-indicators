712c8ba58ba8e874ce9a71dfcc3040ee47cba39b
vsaveris <vsaveris@gmail.com>
2020-09-27 21:55:44 +0200

    feat: Source code project and development branch created.
    Development branch created. Each indicator shall be developed in a
    separate branch and then it should be merged to the development branch.

---
+++ tti/__init__.py
@@ -0,0 +1,13 @@
+"""
+Trading-Technical-Indicators (tti) python library
+
+tti is a python library for calculating more than 140 trading technical indicators from shares data. The library
+provides an API for:
+- trading technical indicators value calculation
+- trading technical indicators graph preparation
+- trading signal calculation
+
+For complete documentation see https://www.trading-technical-indicators.org/
+"""
+
+__version__ = '0.1.dev'
