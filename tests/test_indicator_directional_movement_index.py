37e796edb9af1e0aeb807945f3accec0abf3f986
vsaveris <vsaveris@gmail.com>
2020-10-09 10:04:58 +0200

    test: Directional Movement Index tested.
    The Directional Movement Index technical indicator tested. No changes on
    the code were required.

--- tests/test_indicator_directional_movement_index.py
+++ tests/test_indicator_directional_movement_index.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot
 
 from tti.indicators import DirectionalMovementIndex
+from tti.utils.exceptions import NotEnoughInputData, WrongTypeForInputParameter
 
 
 class TestDirectionalMovementIndex(unittest.TestCase):
@@ -32,27 +33,36 @@ class TestDirectionalMovementIndex(unittest.TestCase):
             DirectionalMovementIndex(df)
 
     def test_input_data_required_column_close_missing(self):
-
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            DirectionalMovementIndex(pd.DataFrame(df.drop(columns=['close'])))
+            DirectionalMovementIndex(pd.DataFrame(
+                df.drop(columns=['close'])))
 
-    def test_input_data_required_column_volume_missing(self):
-        df = pd.read_csv('./data/sample_data.csv',
-                         parse_dates=True, index_col=0)
+    def test_input_data_required_column_high_missing(self):
+        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
+                         index_col=0)
 
         with self.assertRaises(ValueError):
-            DirectionalMovementIndex(pd.DataFrame(df.drop(columns=['volume'])))
+            DirectionalMovementIndex(pd.DataFrame(
+                df.drop(columns=['high'])))
+
+    def test_input_data_required_column_low_missing(self):
+        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
+                         index_col=0)
+
+        with self.assertRaises(ValueError):
+            DirectionalMovementIndex(pd.DataFrame(
+                df.drop(columns=['low'])))
 
     def test_input_data_empty(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            DirectionalMovementIndex(pd.DataFrame(df[df.index >=
-                                                     '2032-01-01']))
+            DirectionalMovementIndex(pd.DataFrame(
+                df[df.index >= '2032-01-01']))
 
     def test_input_data_values_wrong_type(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
@@ -64,6 +74,13 @@ class TestDirectionalMovementIndex(unittest.TestCase):
         with self.assertRaises(ValueError):
             DirectionalMovementIndex(df)
 
+    def test_fill_missing_values_parameter_wrong_type(self):
+        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
+                         index_col=0)
+
+        with self.assertRaises(WrongTypeForInputParameter):
+            DirectionalMovementIndex(df, fill_missing_values=1)
+
     # Validate fill_missing_values input argument
 
     def test_fill_missing_values_is_true(self):
@@ -72,10 +89,10 @@ class TestDirectionalMovementIndex(unittest.TestCase):
 
         df_expected_result = pd.read_csv('./data/missing_values_filled.csv',
                                          parse_dates=True,
-                                         index_col=0)[['close', 'volume']]
+                                         index_col=0)[['high', 'low', 'close']]
 
-        df_result = DirectionalMovementIndex(df, fill_missing_values=True)\
-            ._input_data
+        df_result = DirectionalMovementIndex(
+            df, fill_missing_values=True)._input_data
 
         pd.testing.assert_frame_equal(df_result, df_expected_result)
 
@@ -85,12 +102,10 @@ class TestDirectionalMovementIndex(unittest.TestCase):
 
         df_expected_result = pd.read_csv(
             './data/missing_values_data_sorted.csv', parse_dates=True,
-            index_col=0)[['close', 'volume']]
+            index_col=0)[['high', 'low', 'close']]
 
-        df_result = DirectionalMovementIndex(df, fill_missing_values=False)\
-            ._input_data
-
-        pd.testing.assert_frame_equal(df_result, df_expected_result)
+        with self.assertRaises(ValueError):
+            DirectionalMovementIndex(df, fill_missing_values=False)._input_data
 
     def test_fill_missing_values_is_default_true(self):
         df = pd.read_csv('./data/missing_values_data.csv', parse_dates=True,
@@ -98,7 +113,7 @@ class TestDirectionalMovementIndex(unittest.TestCase):
 
         df_expected_result = pd.read_csv('./data/missing_values_filled.csv',
                                          parse_dates=True,
-                                         index_col=0)[['close', 'volume']]
+                                         index_col=0)[['high', 'low', 'close']]
 
         df_result = DirectionalMovementIndex(df)._input_data
 
@@ -106,27 +121,20 @@ class TestDirectionalMovementIndex(unittest.TestCase):
 
     # Validate indicator creation
 
-    def test_validate_indicator_one_row(self):
+    def test_validate_indicator_less_than_required_data(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
-        df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
-            parse_dates=True,
-            index_col=0)
-
-        df_result = DirectionalMovementIndex(df[df.index == '2000-02-01'])\
-            ._ti_data
-
-        pd.testing.assert_frame_equal(df_expected_result[df_expected_result.
-                                      index == '2000-02-01'], df_result)
+        with self.assertRaises(NotEnoughInputData):
+            DirectionalMovementIndex(df.iloc[:27])
 
     def test_validate_indicator_full_data(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_directional_movement_index_on_' +
+            'sample_data.csv',
             parse_dates=True,
             index_col=0)
 
@@ -145,42 +153,48 @@ class TestDirectionalMovementIndex(unittest.TestCase):
         # Needs manual check of the produced graph
         self.assertEqual(obv.getTiGraph(), matplotlib.pyplot)
 
-        obv.getTiGraph().savefig('./figures/test_on_balance_volume.png')
+        obv.getTiGraph().savefig('./figures/test_directional_movement_' +
+                                 'index.png')
 
     def test_getTiData(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_directional_movement_index_on_' +
+            'sample_data.csv',
             parse_dates=True,
             index_col=0)
 
-        pd.testing.assert_frame_equal(df_expected_result,
-                                      DirectionalMovementIndex(df).getTiData())
+        pd.testing.assert_frame_equal(
+            df_expected_result, DirectionalMovementIndex(df)
+            .getTiData())
 
     def test_getTiValue_specific(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_directional_movement_index_on_' +
+            'sample_data.csv',
             parse_dates=True,
             index_col=0)
 
-        self.assertEqual(df_expected_result.loc['2000-04-25', 'OBV'],
-                         DirectionalMovementIndex(df).getTiValue('2000-04-25'))
+        self.assertEqual(list(df_expected_result.loc['2009-10-19', :]),
+                         DirectionalMovementIndex(df)
+                         .getTiValue('2009-10-19'))
 
     def test_getTiValue_latest(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_directional_movement_index_on_' +
+            'sample_data.csv',
             parse_dates=True,
             index_col=0)
 
-        self.assertEqual(df_expected_result.iloc[-1, 0],
+        self.assertEqual(list(df_expected_result.iloc[-1]),
                          DirectionalMovementIndex(df).getTiValue())
 
     def test_getTiSignal(self):
