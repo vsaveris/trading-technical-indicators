bb9b15caba2e26c8f18ac541307d496bd6f87e1a
vsaveris <vsaveris@gmail.com>
2020-10-11 18:43:14 +0200

    test: Ichimoku Cloud indicator tested.
    Ichimoku Cloud technical indicator tested. No code changes were needed.

--- tests/test_indicator_ichimoku_cloud.py
+++ tests/test_indicator_ichimoku_cloud.py
@@ -10,6 +10,8 @@ import pandas as pd
 import matplotlib.pyplot
 
 from tti.indicators import IchimokuCloud
+from tti.utils.exceptions import NotEnoughInputData, \
+    WrongTypeForInputParameter, WrongValueForInputParameter
 
 
 class TestIchimokuCloud(unittest.TestCase):
@@ -32,19 +34,25 @@ class TestIchimokuCloud(unittest.TestCase):
             IchimokuCloud(df)
 
     def test_input_data_required_column_close_missing(self):
-
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
             IchimokuCloud(pd.DataFrame(df.drop(columns=['close'])))
 
-    def test_input_data_required_column_volume_missing(self):
-        df = pd.read_csv('./data/sample_data.csv',
-                         parse_dates=True, index_col=0)
+    def test_input_data_required_high_close_missing(self):
+        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
+                         index_col=0)
 
         with self.assertRaises(ValueError):
-            IchimokuCloud(pd.DataFrame(df.drop(columns=['volume'])))
+            IchimokuCloud(pd.DataFrame(df.drop(columns=['high'])))
+
+    def test_input_data_required_column_low_missing(self):
+        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
+                         index_col=0)
+
+        with self.assertRaises(ValueError):
+            IchimokuCloud(pd.DataFrame(df.drop(columns=['low'])))
 
     def test_input_data_empty(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
@@ -63,6 +71,13 @@ class TestIchimokuCloud(unittest.TestCase):
         with self.assertRaises(ValueError):
             IchimokuCloud(df)
 
+    def test_fill_missing_values_parameter_wrong_type(self):
+        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
+                         index_col=0)
+
+        with self.assertRaises(WrongTypeForInputParameter):
+            IchimokuCloud(df, fill_missing_values=1)
+
     # Validate fill_missing_values input argument
 
     def test_fill_missing_values_is_true(self):
@@ -71,7 +86,7 @@ class TestIchimokuCloud(unittest.TestCase):
 
         df_expected_result = pd.read_csv('./data/missing_values_filled.csv',
                                          parse_dates=True,
-                                         index_col=0)[['close', 'volume']]
+                                         index_col=0)[['high', 'low', 'close']]
 
         df_result = IchimokuCloud(df, fill_missing_values=True)._input_data
 
@@ -83,7 +98,7 @@ class TestIchimokuCloud(unittest.TestCase):
 
         df_expected_result = pd.read_csv(
             './data/missing_values_data_sorted.csv', parse_dates=True,
-            index_col=0)[['close', 'volume']]
+            index_col=0)[['high', 'low', 'close']]
 
         df_result = IchimokuCloud(df, fill_missing_values=False)._input_data
 
@@ -95,7 +110,7 @@ class TestIchimokuCloud(unittest.TestCase):
 
         df_expected_result = pd.read_csv('./data/missing_values_filled.csv',
                                          parse_dates=True,
-                                         index_col=0)[['close', 'volume']]
+                                         index_col=0)[['high', 'low', 'close']]
 
         df_result = IchimokuCloud(df)._input_data
 
@@ -103,26 +118,12 @@ class TestIchimokuCloud(unittest.TestCase):
 
     # Validate indicator creation
 
-    def test_validate_indicator_one_row(self):
-        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
-                         index_col=0)
-
-        df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
-            parse_dates=True,
-            index_col=0)
-
-        df_result = IchimokuCloud(df[df.index == '2000-02-01'])._ti_data
-
-        pd.testing.assert_frame_equal(df_expected_result[df_expected_result.
-                                      index == '2000-02-01'], df_result)
-
     def test_validate_indicator_full_data(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_ichimoku_cloud_on_sample_data.csv',
             parse_dates=True,
             index_col=0)
 
@@ -141,42 +142,43 @@ class TestIchimokuCloud(unittest.TestCase):
         # Needs manual check of the produced graph
         self.assertEqual(obv.getTiGraph(), matplotlib.pyplot)
 
-        obv.getTiGraph().savefig('./figures/test_on_balance_volume.png')
+        obv.getTiGraph().savefig('./figures/test_moving_average.png')
 
     def test_getTiData(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_ichimoku_cloud_on_sample_data.csv',
             parse_dates=True,
             index_col=0)
 
-        pd.testing.assert_frame_equal(df_expected_result,
-                                      IchimokuCloud(df).getTiData())
+        pd.testing.assert_frame_equal(df_expected_result.round(4),
+                                      IchimokuCloud(df)
+                                      .getTiData())
 
     def test_getTiValue_specific(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
+            './data/test_ichimoku_cloud_on_sample_data.csv',
             parse_dates=True,
             index_col=0)
 
-        self.assertEqual(df_expected_result.loc['2000-04-25', 'OBV'],
-                         IchimokuCloud(df).getTiValue('2000-04-25'))
+        self.assertEqual(list(df_expected_result.loc['2009-10-19', :]),
+                         IchimokuCloud(df).
+                         getTiValue('2009-10-19'))
 
     def test_getTiValue_latest(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_on_balance_volume_on_sample_data.csv',
-            parse_dates=True,
-            index_col=0)
+            './data/test_ichimoku_cloud_on_sample_data.csv',
+            parse_dates=True,  index_col=0).round(4)
 
-        self.assertEqual(df_expected_result.iloc[-1, 0],
+        self.assertEqual(list(df_expected_result.iloc[-1]),
                          IchimokuCloud(df).getTiValue())
 
     def test_getTiSignal(self):
