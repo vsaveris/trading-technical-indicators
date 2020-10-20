53475e928cce02acd167c2cdcffbed5d7638c9ce
vsaveris <vsaveris@gmail.com>
2020-10-21 01:05:16 +0200

    test: Chaikin Oscillator indicator tested.
    Chaikin Oscillator technical indicator tested. No updates in the code.
    Ready for release.

--- tests/test_indicator_chaikin_oscillator.py
+++ tests/test_indicator_chaikin_oscillator.py
@@ -1,72 +1,72 @@
 """
 Trading-Technical-Indicators (tti) python library
 
-File name: test_indicator_chaikin_money_flow.py
-    tti.indicators package, _chaikin_money_flow.py> module unit tests.
+File name: test_indicator_chaikin_oscillator.py
+    tti.indicators package, _chaikin_oscillator.py module unit tests.
 """
 
 import unittest
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from tti.indicators import ChaikinMoneyFlow
+from tti.indicators import ChaikinOscillator
 from tti.utils.exceptions import NotEnoughInputData, \
-    WrongTypeForInputParameter, WrongValueForInputParameter
+    WrongTypeForInputParameter
 
 
-class TestChaikinMoneyFlow(unittest.TestCase):
+class TestChaikinOscillator(unittest.TestCase):
 
     # Validate input_data parameter
 
     def test_input_data_missing(self):
         with self.assertRaises(TypeError):
-            ChaikinMoneyFlow()
+            ChaikinOscillator()
 
     def test_input_data_wrong_type(self):
         with self.assertRaises(TypeError):
-            ChaikinMoneyFlow('NO_DF')
+            ChaikinOscillator('NO_DF')
 
     def test_input_data_wrong_index_type(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=1)
 
         with self.assertRaises(TypeError):
-            ChaikinMoneyFlow(df)
+            ChaikinOscillator(df)
 
     def test_input_data_required_column_close_missing(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            ChaikinMoneyFlow(pd.DataFrame(df.drop(columns=['close'])))
+            ChaikinOscillator(pd.DataFrame(df.drop(columns=['close'])))
 
     def test_input_data_required_column_high_missing(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            ChaikinMoneyFlow(pd.DataFrame(df.drop(columns=['high'])))
+            ChaikinOscillator(pd.DataFrame(df.drop(columns=['high'])))
 
     def test_input_data_required_column_low_missing(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            ChaikinMoneyFlow(pd.DataFrame(df.drop(columns=['low'])))
+            ChaikinOscillator(pd.DataFrame(df.drop(columns=['low'])))
 
     def test_input_data_required_column_volume_missing(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            ChaikinMoneyFlow(pd.DataFrame(df.drop(columns=['volume'])))
+            ChaikinOscillator(pd.DataFrame(df.drop(columns=['volume'])))
 
     def test_input_data_empty(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(ValueError):
-            ChaikinMoneyFlow(pd.DataFrame(df[df.index >= '2032-01-01']))
+            ChaikinOscillator(pd.DataFrame(df[df.index >= '2032-01-01']))
 
     def test_input_data_values_wrong_type(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
@@ -76,28 +76,14 @@ class TestChaikinMoneyFlow(unittest.TestCase):
         df['close'].iat[0] = 'no-numeric'
 
         with self.assertRaises(ValueError):
-            ChaikinMoneyFlow(df)
-
-    def test_period_parameter_wrong_type(self):
-        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
-                         index_col=0)
-
-        with self.assertRaises(WrongTypeForInputParameter):
-            ChaikinMoneyFlow(df, period='30')
-
-    def test_period_parameter_wrong_value(self):
-        df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
-                         index_col=0)
-
-        with self.assertRaises(WrongValueForInputParameter):
-            ChaikinMoneyFlow(df, period=0)
+            ChaikinOscillator(df)
 
     def test_fill_missing_values_parameter_wrong_type(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(WrongTypeForInputParameter):
-            ChaikinMoneyFlow(df, period=200, fill_missing_values=1)
+            ChaikinOscillator(df, fill_missing_values=1)
 
     # Validate fill_missing_values input argument
 
@@ -110,8 +96,7 @@ class TestChaikinMoneyFlow(unittest.TestCase):
                                          index_col=0)[['high', 'low', 'close',
                                                        'volume']]
 
-        df_result = ChaikinMoneyFlow(df, period=20, fill_missing_values=True).\
-            _input_data
+        df_result = ChaikinOscillator(df, fill_missing_values=True)._input_data
 
         pd.testing.assert_frame_equal(df_result, df_expected_result)
 
@@ -123,7 +108,7 @@ class TestChaikinMoneyFlow(unittest.TestCase):
             './data/missing_values_data_sorted.csv', parse_dates=True,
             index_col=0)[['high', 'low', 'close', 'volume']]
 
-        df_result = ChaikinMoneyFlow(df, period=20, fill_missing_values=False)\
+        df_result = ChaikinOscillator(df, fill_missing_values=False) \
             ._input_data
 
         pd.testing.assert_frame_equal(df_result, df_expected_result)
@@ -137,7 +122,7 @@ class TestChaikinMoneyFlow(unittest.TestCase):
                                          index_col=0)[['high', 'low', 'close',
                                                        'volume']]
 
-        df_result = ChaikinMoneyFlow(df, period=20)._input_data
+        df_result = ChaikinOscillator(df)._input_data
 
         pd.testing.assert_frame_equal(df_result, df_expected_result)
 
@@ -148,25 +133,25 @@ class TestChaikinMoneyFlow(unittest.TestCase):
                          index_col=0)
 
         with self.assertRaises(NotEnoughInputData):
-            ChaikinMoneyFlow(df[df.index == '2000-02-01'])
+            ChaikinOscillator(df[df.index == '2000-02-01'])
 
     def test_validate_indicator_less_than_required_data(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         with self.assertRaises(NotEnoughInputData):
-            ChaikinMoneyFlow(df.iloc[:13], period=14)
+            ChaikinOscillator(df.iloc[:8])
 
     def test_validate_indicator_full_data(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_chaikin_money_flow_on_sample_data.csv',
+            './data/test_chaikin_oscillator_on_sample_data.csv',
             parse_dates=True,
             index_col=0).round(4)
 
-        df_result = ChaikinMoneyFlow(df)._ti_data
+        df_result = ChaikinOscillator(df)._ti_data
 
         pd.testing.assert_frame_equal(df_expected_result, df_result)
 
@@ -176,12 +161,12 @@ class TestChaikinMoneyFlow(unittest.TestCase):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
-        obv = ChaikinMoneyFlow(df)
+        obv = ChaikinOscillator(df)
 
         # Needs manual check of the produced graph
         self.assertEqual(obv.getTiGraph(), plt)
 
-        obv.getTiGraph().savefig('./figures/test_chaikin_money_flow.png')
+        obv.getTiGraph().savefig('./figures/test_chaikin_oscillator.png')
         plt.close('all')
 
     def test_getTiData(self):
@@ -189,12 +174,12 @@ class TestChaikinMoneyFlow(unittest.TestCase):
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_chaikin_money_flow_on_sample_data.csv',
+            './data/test_chaikin_oscillator_on_sample_data.csv',
             parse_dates=True,
             index_col=0)
 
         pd.testing.assert_frame_equal(df_expected_result.round(4),
-                                      ChaikinMoneyFlow(df)
+                                      ChaikinOscillator(df)
                                       .getTiData())
 
     def test_getTiValue_specific(self):
@@ -202,12 +187,12 @@ class TestChaikinMoneyFlow(unittest.TestCase):
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_chaikin_money_flow_on_sample_data.csv',
+            './data/test_chaikin_oscillator_on_sample_data.csv',
             parse_dates=True,
             index_col=0).round(4)
 
         self.assertEqual(list(df_expected_result.loc['2009-10-19', :]),
-                         ChaikinMoneyFlow(df).
+                         ChaikinOscillator(df).
                          getTiValue('2009-10-19'))
 
     def test_getTiValue_latest(self):
@@ -215,18 +200,18 @@ class TestChaikinMoneyFlow(unittest.TestCase):
                          index_col=0)
 
         df_expected_result = pd.read_csv(
-            './data/test_chaikin_money_flow_on_sample_data.csv',
+            './data/test_chaikin_oscillator_on_sample_data.csv',
             parse_dates=True,
             index_col=0).round(4)
 
         self.assertEqual(list(df_expected_result.iloc[-1]),
-                         ChaikinMoneyFlow(df).getTiValue())
+                         ChaikinOscillator(df).getTiValue())
 
     def test_getTiSignal(self):
         df = pd.read_csv('./data/sample_data.csv', parse_dates=True,
                          index_col=0)
 
-        self.assertIn(ChaikinMoneyFlow(df).getTiSignal(),
+        self.assertIn(ChaikinOscillator(df).getTiSignal(),
                       [('buy', -1), ('hold', 0), ('sell', 1)])
 
 
