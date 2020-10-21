f0ce5c5b78d17d1460ee0da0a54b7daa753b3729
vsaveris <vsaveris@gmail.com>
2020-10-21 19:39:58 +0200

    style: Set max length for each line to 80 characters.
    Long lines (>80) characters refactored.

--- tti/__init__.py
+++ tti/__init__.py
@@ -1,7 +1,8 @@
 """
 Trading-Technical-Indicators (tti) python library
 
-tti is a python library for calculating more than 140 trading technical indicators from shares data. The library
+tti is a python library for calculating more than 140 trading technical
+indicators from stocks data. The library
 provides an API for:
 - trading technical indicators value calculation
 - trading technical indicators graph preparation
