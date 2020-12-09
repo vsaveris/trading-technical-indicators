e06098fe92adffe865a6954875f0d61922b93a0a
vsaveris <vsaveris@gmail.com>
2020-12-09 22:02:57 +0100

    feat: _trading_simulation module introduced in the tti.utils package.
    TradingSimulation class introduced, in where util methods will be
    implemented for the runSimulation method of the tti.indicators.package.

---
+++ tti/utils/_trading_simulation.py
@@ -0,0 +1,17 @@
+"""
+Trading-Technical-Indicators (tti) python library
+
+File name: _trading_simulation.py
+    Trading simulation class implementation defined under the tti.utils
+    package.
+"""
+
+
+class TradingSimulation:
+    """
+    Trading Simulation class implementation. Provides utilities methods for
+    the runSimulation method of the tti.indicators package.
+    """
+
+    def __init__(self):
+        pass
